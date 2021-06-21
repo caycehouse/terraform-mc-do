ce392f742c9bdda2818ae2c60599441fd0aeb0a4
Cayce House <cayce@mobilemedics.com>
2021-06-21 04:51:29 +0000

    Fixed bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,8 +6,9 @@ docker run -d \
     --restart always \
     -p 25565:25565/tcp \
     -v /mnt/minecraft:/data \
-    -e EULA="TRUE" \
-    -e MEMORY="6144M" \
+    -e EULA=true \
+    -e MEMORY=3072M \
     -e TYPE="SPIGOT" \
-    -e USE_AIKAR_FLAGS="TRUE" \
+    -e USE_AIKAR_FLAGS=true \
+    -e FORCE_REDOWNLOAD=true \
     itzg/minecraft-server
